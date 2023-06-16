9f97f9d121be233a1a9ce1f6e08d04c5a19b0b18
cliffboa <cliffodhiambo1@gmail.com>
2023-06-16 04:26:31 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include &lt;stdio.h&gt;
+#include &lt;unistd.h&gt;
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+write(2,
+&quot;and that piece of art is useful\&quot; - Dora Korpar, 2015-10-19\n&quot;, 59);
+return (1);
+
+}
