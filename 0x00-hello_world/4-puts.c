982cd16d5effef40fea03e233f9523f4c928b7eb
cliffboa <cliffodhiambo1@gmail.com>
2023-06-16 05:20:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return (0);
+    char *str = "Programming is like building a multilingual puzzle\n";
+    puts(str);
+    return (0);
 }
-
