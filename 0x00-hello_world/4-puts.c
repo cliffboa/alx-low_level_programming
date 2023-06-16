6b0d4cf99a0e221a530caf30af31b583fef1a2ba
cliffboa <cliffodhiambo1@gmail.com>
2023-06-16 05:17:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
-#include&lt;stdio.h&gt;
+#include <stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-puts(&quot;\&quot;Programming is like building a multilingual puzzle&quot;);
-return (0);
+	puts("Programming is like building a multilingual puzzle");
+	return (0);
 }
+
