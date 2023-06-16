cbd0678fe788cc3a54d43fdd0177f670e6591027
cliffboa <cliffodhiambo1@gmail.com>
2023-06-16 04:19:36 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include&lt;stdio.h&gt;
+/**
+
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+printf(&quot;with proper grammar, but the outcome is a piece of art,\n&quot;);
+return (0);
+}
