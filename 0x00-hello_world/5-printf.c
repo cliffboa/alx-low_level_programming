82437d3ba9222a0df25fe2b90228a2d331510216
cliffboa <cliffodhiambo1@gmail.com>
2023-06-16 04:42:37 -0700

    Add 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
-#include&lt;stdio.h&gt;
-/**
+#include <stdio.h>
 
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-printf(&quot;with proper grammar, but the outcome is a piece of art,\n&quot;);
-return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
 }
+
