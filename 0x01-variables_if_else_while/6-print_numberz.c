e2f282e9314c6bedaf38daa6a572e697995b9c6b
cliffboa <cliffodhiambo1@gmail.com>
2023-06-19 16:52:18 -0700

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -10,15 +10,15 @@
  */
 int main(void)
 {
-	int num;
+    int num;
 
-	for (num = 0; num < 10; num++)
-	{
-		putchar(num + '0');
-	}
+    for (num = 0; num < 10; num++)
+    {
+        putchar(num + '0');
+    }
 
-	putchar('\n');
+    putchar('\n');
 
-	return (0);
+    return (0);
 }
 
