abfcaabea9d26aefd41ac7995bef4685450f8b76
cliffboa <cliffodhiambo1@gmail.com>
2023-06-19 13:27:33 -0700

    0-positive_or_negative.c

--- 0-positive_or_negative.c/0-positive_or_negative.c
+++ 0-positive_or_negative.c/0-positive_or_negative.c
@@ -1,10 +1,9 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Determines if a number is positive, negative, or zero.
- *
+ *Description: 'Check for the number's positivity or negativity'
  * Return: Always 0 (Success)
  */
 int main(void)
