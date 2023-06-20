dc6ac0c1e7c885d3c20bdf7d9e72b5ffa4533b1f
cliffboa <cliffodhiambo1@gmail.com>
2023-06-20 06:24:18 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar\n";
+	int i = 0;
+
+	while (text[i] != '\0')
+	{
+		write(1, &text[i], 1);
+		i++;
+	}
+
+	return (0);
+}
+
