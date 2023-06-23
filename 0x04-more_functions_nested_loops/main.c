8bd9a3628f0cd324fd217eb7f38a13505d2e9805
cliffboa <cliffodhiambo1@gmail.com>
2023-06-23 12:28:11 -0700

    6-lines

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -3,6 +3,30 @@
 int main(void)
 {
     print_numbers();
+    print_line(0);
     return 0;
+}
+
+void print_numbers(void)
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        _putchar(i + '0');
+    }
+    _putchar('\n');
+}
+
+void print_line(int n)
+{
+    int i;
+    if (n > 0)
+    {
+        for (i = 0; i < n; i++)
+        {
+            _putchar('_');
+        }
+    }
+    _putchar('\n');
 }
 
