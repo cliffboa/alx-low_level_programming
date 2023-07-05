d4b53bcf9edeb031c410c9dfab9342504feb3e6c
cliffboa <cliffodhiambo1@gmail.com>
2023-07-05 07:41:59 -0700

    6-is_prime_number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,11 +1,11 @@
 /**
- * _check_prime - Recursive helper function to check if a number is prime
+ * check_prime - Recursive helper function to check if a number is prime
  * @n: The number to check
  * @div: The current divisor to check
  *
  * Return: 1 if the number is prime, 0 otherwise
  */
-int _check_prime(int n, int div);
+int check_prime(int n, int div);
 
 /**
  * is_prime_number - Checks if a number is a prime number
@@ -15,24 +15,24 @@ int _check_prime(int n, int div);
  */
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
-    return (_check_prime(n, 2));
+	if (n <= 1)
+		return (0);
+	return (check_prime(n, 2));
 }
 
 /**
- * _check_prime - Recursive helper function to check if a number is prime
+ * check_prime - Recursive helper function to check if a number is prime
  * @n: The number to check
  * @div: The current divisor to check
  *
  * Return: 1 if the number is prime, 0 otherwise
  */
-int _check_prime(int n, int div)
+int check_prime(int n, int div)
 {
-    if (n % div == 0)
-        return (0);
-    if (div * div > n)
-        return (1);
-    return (_check_prime(n, div + 1));
+	if (n % div == 0)
+		return (0);
+	if (div * div > n)
+		return (1);
+	return (check_prime(n, div + 1));
 }
 
