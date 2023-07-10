4c3e622aa2a26a14584ef6225442db8f52df6d52
cliffboa <cliffodhiambo1@gmail.com>
2023-07-10 07:01:34 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of strings containing the command-line arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv; /* Indicate that argv is unused */
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
