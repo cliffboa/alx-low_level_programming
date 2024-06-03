15e8eba35dae5c02672ac96e89f3965e65439fec
Cliffboa <cliffodhiambo1@gmail.com>
2024-06-03 03:07:21 -0700

    5-island

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module to calculate the perimeter of an island in a grid.
+"""
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+    
+    Args:
+    grid (list of list of int): The grid representing the map where
+                                0 represents water and 1 represents land.
+    
+    Returns:
+    int: The perimeter of the island.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check top
+                if i == 0 or grid[i - 1][j] == 0:
+                    perimeter += 1
+                # Check bottom
+                if i == rows - 1 or grid[i + 1][j] == 0:
+                    perimeter += 1
+                # Check left
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                # Check right
+                if j == cols - 1 or grid[i][j + 1] == 0:
+                    perimeter += 1
+    
+    return perimeter
+
